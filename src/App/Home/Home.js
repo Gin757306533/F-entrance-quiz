1d83d8f9ddb7390282df87a4c8085fc62c1ccbd7
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-11-02 01:22:17 +0800

    [mengqiu] add comments

--- src/App/Home/Home.js
+++ src/App/Home/Home.js
@@ -1,11 +1,13 @@
 // eslint-disable-next-line no-unused-vars,no-undef
 import React from 'react';
 
+// TODO GTB-工程实践: - 不应该disable eslint哈，如果有eslint error应该要去fix error
 class Home extends React.PureComponent {
   // eslint-disable-next-line constructor-super,no-unused-vars,no-useless-constructor
   constructor(props) {
     super(props);
   }
+// TODO GTB-工程实践: - css class的命名格式不统一，且不正确。正确的规范是a-b这种格式。
 
   render() {
     return (
